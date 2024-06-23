34232e12a930d9b71d9d3ad51fe2e08cbf599fd5
Török Ádám <adam-torok@outlook.hu>
2024-06-23 14:24:04 +0200

    #05 Style the current navigation link

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
 
 <head>
     <meta charset="utf-8">
@@ -8,7 +8,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<div class="min-h-full">
+<body class="min-h-full">
     <nav class="bg-gray-800">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div class="flex h-16 items-center justify-between">
@@ -17,14 +17,12 @@
                         <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                             alt="Your Company">
                     </div>
-                    <div class="hidden md:block">
+                    <div>
                         <div class="ml-10 flex items-baseline space-x-4">
-                            <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                                aria-current="page">Dashboard</a>
-                            <a href="/about"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                            <a href="/contact"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                            <x-nav-link :active="request()->is('/')" href="/">Dashboard</x-nav-link>
+                            <x-nav-link :active="request()->is('about')" href="/about">About</x-nav-link>
+                            <x-nav-link :active="request()->is('contact')" href="/contact">Contact</x-nav-link>
+                            <x-nav-link type="button" :active="request()->is('login')" href="/contact">Login</x-nav-link>
                         </div>
                     </div>
                 </div>
@@ -45,6 +43,6 @@
             {{ $slot }}
         </div>
     </main>
-</div>
+</body>
 
 </html>
